d296f295b3c9aaf4372a1027d4225239cec44518
Sergey Vorobey <sergey88nik@gmail.com>
2022-10-20 20:39:38 +0300

    add burger ang fixed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,43 @@
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+$(document).ready(function(){
+  new WOW().init({
+    animateClass: 'animate__animated'
+  });
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+  $('#slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }]
+  });
+
+
+  $('#popup-trigger').on('click', () => {
+    $('#popup').show();
+    // fadeIn
+  })
+  $('#popup-hide').on('click', () => {
+    $('#popup').hide();
+    // fadeOut
+  })
+
+  $('#menu-trigger').on('click', () => {
+    $('#menu-mobile').show();
+    // fadeIn
+  })
+  $('#menu-hide').on('click', () => {
+    $('#menu-mobile').hide();
+    // fadeOut
+  })
+
+
+});
+
